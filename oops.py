143b1e0f4a69c2155f5b82acae9555a8bba95fa3
sengathirmu-glitch <sengathir.mu@gmail.com>
2026-02-21 10:56:43 +0100

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,35 @@
+# class a():
+     
+#      def displayName(self) :
+#           print("Sengathir")
+
+#      age = 14
+
+
+# myobject = a()
+# print(myobject.age)
+# print(myobject.displayName())
+
+
+
+# class b():
+
+#     def types(self):
+#         print("samsung") 
+
+#     cost = 5000 
+
+# myObject2 = b()
+# print(myObject2.cost)
+# print(myObject2.types())
+
+
+
+
+
+
+
+
+
+
+
